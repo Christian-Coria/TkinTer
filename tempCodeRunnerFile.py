dd83bef244651faeaf44973d84459a5ba92c5a09
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-10-30 20:15:11 -0300

    app login

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-    messagebox.showinfo('mostramos informacion', mensaje1 + ' informativo')
-        messagebox.showerror('Mensaje de error', mensaje1 + ' Error')
-        messagebox.showwarning('Mensaje de cuidado', mensaje1 + ' Alerta')
+mensaje2
--- ventana_login.py
+++ ventana_login.py
@@ -1,13 +1,32 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 ventana = tk.Tk()
-ventana.geometry('250x150')
+ventana.geometry('300x130')
 ventana.title('Login')
 ventana.iconbitmap('soporte.ico')
+ventana.resizable(0,0) #fijamos a un unico tamaño no modificable
 
+ventana.columnconfigure(0, weight=1) #fijamos espacio de columnas
+ventana.columnconfigure(1, weight=3)
 
+etiqueta1 = tk.Label(ventana, text='Usuario: ')
+etiqueta1.grid(row=0, column=0, sticky=tk.E, padx=5 , pady=5)
+entrada1 = ttk.Entry(ventana) 
+entrada1.grid(row=0, column=1, sticky=tk.W, padx=5, pady=5)
 
+etiqueta2 = tk.Label(ventana, text='Contraseña')
+etiqueta2.grid(row=1, column=0, sticky=tk.E, padx=5 , pady=5)
+entrada2 = ttk.Entry(ventana, show='*') 
+entrada2.grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
 
+def login():
+    
+    messagebox.showinfo('Datos Login',
+                        f' Usuario: {entrada1.get()}, Password: {entrada2.get()}')  
+    
+
+boton1 = ttk.Button(ventana, text='login', command=login)
+boton1.grid(row=3, column=0, columnspan=2)
 
 ventana.mainloop()
